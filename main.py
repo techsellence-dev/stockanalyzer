76ade7ce94bac71931adc3fe4bbc7baa1c02920f
Sohini Majumdar <msohini@amazon.com>
2023-06-09 15:36:32 +0530

    Added plot for Stock Price - Open, High, Low and Close

--- main.py
+++ main.py
@@ -6,15 +6,17 @@
 import pandas as pd
 import yfinance as yf
 import os
+import matplotlib.pyplot as plt
 
 
-def download_stock_data(stock_name):
-    historical_data = yf.download(stock_name)
-    output_dir = os.getcwd() + '/StockData/Output/' + stock_name
+def download_stock_data(stock_symbol):
+    historical_data = yf.download(stock_symbol)
+    output_dir = os.getcwd() + '/StockData/Output/' + stock_symbol
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     historical_data.to_csv(os.path.join(output_dir, 'historical_data'))
-    print('Successfully downloaded stock data for symbol: ' + stock_name)
+    print('Successfully downloaded stock data for symbol: ' + stock_symbol)
+
 
 def deduplicate_stock_symbols():
     nse_stocks = pd.read_csv(os.getcwd() + '/StockData/Input/NSE_Stocks.csv')
@@ -32,7 +34,8 @@ def deduplicate_stock_symbols():
     nse_stock_symbols.rename(columns={'Symbol': 'Stock Symbol'}, inplace=True)
     bse_stock_symbols.rename(columns={'Issuer Name': 'Stock Symbol'}, inplace=True)
     concatenated_stock_symbols = pd.concat([nse_stock_symbols, bse_stock_symbols])
-    deduped_stock_symbols = concatenated_stock_symbols.drop_duplicates(subset = ['Stock Symbol'], keep = 'first').reset_index(drop = True)
+    deduped_stock_symbols = concatenated_stock_symbols.drop_duplicates(subset=['Stock Symbol'],
+                                                                       keep='first').reset_index(drop=True)
     print("Total number of stocks after concatenation and deduplication: " + str(deduped_stock_symbols.shape[0]))
     deduped_stock_symbols.to_csv(os.getcwd() + '/StockData/Output/DedupedStockSymbols')
 
@@ -46,7 +49,25 @@ def download_historical_stock_data():
             download_stock_data(stocks_df['Stock Symbol'][ind] + '.BO')
 
 
+def plot_individual_stock(stock_symbol):
+    output_dir = os.getcwd() + '/StockData/Output/' + stock_symbol
+    output_data_path = output_dir + '/historical_data'
+    output_plot_path = output_dir + '/' + stock_symbol + '.png'
+    historical_stock_df = pd.read_csv(output_data_path)
+    historical_stock_df.plot(x='Date', y=['Open', 'High', 'Low', 'Close'])
+    plt.savefig(output_plot_path)
+    print('Successfully saved historical stock data plot')
+
+
+def plot_and_save_historical_stock_data():
+    stocks_df = pd.read_csv(os.getcwd() + '/StockData/Output/DedupedStockSymbols')
+    for ind in stocks_df.index:
+        if stocks_df['Source'][ind] == 'NSE':
+            plot_individual_stock(stocks_df['Stock Symbol'][ind] + '.NS')
+        elif stocks_df['Source'][ind] == 'BSE':
+            plot_individual_stock(stocks_df['Stock Symbol'][ind] + '.BO')
+
 if __name__ == '__main__':
     deduplicate_stock_symbols()
     download_historical_stock_data()
-
+    plot_and_save_historical_stock_data()
